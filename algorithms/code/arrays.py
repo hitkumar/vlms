9a7a0946bdddab814a904551b8ea177442b019f8
hitkumar <hitesh.nahan@gmail.com>
2025-09-07 23:11:20 -0700

    [code] Array problems

--- algorithms/code/arrays.py
+++ algorithms/code/arrays.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, deque, heapq
 from typing import List
 
 
@@ -51,4 +52,63 @@ class ArrayProblems:
             if target - num in numbers:
                 return [numbers[target - num], i]
             numbers[num] = i
+        return []
+
+    def sorted_str(self, s: str):
+        return "".join(sorted(s))
+
+    def sort_efficient(self, s: str):
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+        return tuple(count)
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for s in strs:
+            str_hash = self.sorted_str(s)
+            if str_hash in anagrams:
+                anagrams[str_hash].append(s)
+            else:
+                anagrams[str_hash] = [s]
+
+        return [v for k, v in anagrams.items()]
+
+    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = self.sorted_str(s)
+            res[sortedS].append(s)
+        return list(res.values())
+
+    def topKFrequent_Heap(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        heap = []
+        for num, freq in count.items():
+            heapq.heappush(heap, (freq, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Bucket sort approach in O(n) time
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        freqs = [[] for i in range(len(nums) + 1)]
+        for num, cnt in count.items():
+            freqs[cnt].append(num)
+        res = []
+        for i in range(len(freqs) - 1, 0, -1):
+            for num in freqs[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
         return []
